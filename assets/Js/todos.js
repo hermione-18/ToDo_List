4b1d3522559135a73bc205127fb926afdeb1affe
roshni_18 <45714224+hermione-18@users.noreply.github.com>
2019-07-12 23:21:58 +0530

    Update todos.js

--- assets/Js/todos.js
+++ assets/Js/todos.js
@@ -1,1 +1,22 @@
+//toggle todo completion
+$("ul").on("click", "li", function() {
+    $(this).toggleClass("completed");
+});
+$("ul").on("click", "span", function(e) {
+    $(this).parent().fadeOut(300, function() {
+        $(this).remove();
+    });
+    e.stopPropagation();
+});
 
+$("input[type='text'").keypress(function(e) {
+    if (e.which === 13) {
+        var newtext = $(this).val(); //extrace new todo item
+        $("ul").append("<li><span><i class='fa fa-minus'></i></span> " + newtext + "</li>"); //add new todo to the list
+        $(this).val("");
+    }
+})
+
+$(".fa-plus").click(function() {
+    $("input[type='text'").fadeToggle();
+});
